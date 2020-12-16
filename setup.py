e32757b8fae7e792b38efb17d6c64c074d6ff86a
Kajaste <Kajaste@users.noreply.github.com>
2020-12-16 22:16:27 +0200

    Bump upcoming version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pychoir",
-    version="0.0.2",
+    version="0.0.3",
     author="Antti Kajander",
     author_email="antti.kajander@gmail.com",
     description="Matcher templates for humans",
