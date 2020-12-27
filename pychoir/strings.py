042ab0fc3c35d796dcbd95c7dc1ba405817d9feb
Kajaste <Kajaste@users.noreply.github.com>
2020-12-28 01:03:31 +0200

    Add MatchesRegex Matcher for strings

--- pychoir/strings.py
+++ pychoir/strings.py
@@ -1,4 +1,13 @@
+import re
+import sys
+from typing import Any, Union
+
 from pychoir import Matcher
+
+if sys.version_info >= (3, 7):
+    from re import Pattern
+else:
+    Pattern = Any
 
 
 class StartsWith(Matcher):
@@ -23,3 +32,17 @@ class EndsWith(Matcher):
 
     def _description(self) -> str:
         return repr(self.end)
+
+
+class MatchesRegex(Matcher):
+    def __init__(self, regex: Union[str, Pattern]):
+        super().__init__()
+        if isinstance(regex, str):
+            regex = re.compile(regex)
+        self.regex = regex
+
+    def _matches(self, other: str) -> bool:
+        return re.match(self.regex, other) is not None
+
+    def _description(self) -> str:
+        return repr(self.regex)
--- tests/test_strings.py
+++ tests/test_strings.py
@@ -1,11 +1,26 @@
+import re
+
 from pychoir import EndsWith, StartsWith
+from pychoir.strings import MatchesRegex
 
 
 def test_startswith():
     assert 'foobar' == StartsWith('foo')
     assert 'barbar' != StartsWith('foo')
 
+    assert str(StartsWith('foo')) == "StartsWith('foo')"
+
 
 def test_endswith():
     assert 'foobar' == EndsWith('bar')
     assert 'foo' != EndsWith('bar')
+
+    assert str(EndsWith('bar')) == "EndsWith('bar')"
+
+
+def test_matches_regex():
+    assert 'foobar' == MatchesRegex(r'^foo')
+    assert 'foobar' == MatchesRegex(re.compile(r'^foo.ar$', flags=re.VERBOSE))
+    assert 'bafoo' != MatchesRegex(r'^foo')
+
+    assert str(MatchesRegex(re.compile(r'^foo', re.VERBOSE))) == "MatchesRegex(re.compile('^foo', re.VERBOSE))"
